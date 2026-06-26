718806a03589bcb1502b7601a4122f891611b88c
majidalimajid561 <majid.official561@gmail.com>
2026-06-27 00:07:59 +0500

    Remove unnecessary blank line in app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score
 
-
 # ─────────────────────────────────────────
 # 1. PAGE  CONFIG
 # ─────────────────────────────────────────
